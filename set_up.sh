b223aad244486930153ad7064959bf2707edc98c
lfm <lfm>
2019-07-16 10:28:08 +0800

    stage

---
+++ set_up.sh
@@ -0,0 +1,16 @@
+# docker 
+sudo apt-get update
+sudo apt-get install docker 
+sudo apt-get install docker-compose
+sudo apt-get install node
+
+mvn clean
+cd server
+docker-compose up -d
+cd ../client
+npm install && npm run build
+cd ..
+mvn package
+cd server/target/
+java -jar stc.jar
+cd ../../
