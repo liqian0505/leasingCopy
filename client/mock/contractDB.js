95ac5d6422e049e73389b17943f12d079dda59ab
徐蕴智 <151220134@smail.nju.edu.cn>
2019-07-16 15:21:23 +0800

    add mock data

--- client/mock/contractDB.js
+++ client/mock/contractDB.js
@@ -1,4 +1,29 @@
-const contractData = [];
+const contractData = [
+  {
+    id: 1,
+    name: '模板一',
+    style: '样式一',
+    formData: {
+
+    },
+  },
+  {
+    id: 2,
+    name: '模板二',
+    style: '样式二',
+    formData: {
+
+    },
+  },
+  {
+    id: 3,
+    name: '模板三',
+    style: '样式三',
+    formData: {
+
+    },
+  },
+];
 
 export default {
   'GET /contract': (req, res) => {},
--- client/mock/templateDB.js
+++ client/mock/templateDB.js
@@ -3,15 +3,58 @@ const templateData = [
     id: 1,
     name: '模板一',
     style: '样式一',
+    editorState: {
+      blocks: [{
+        key: '3b1pq',
+        text: 'empty',
+        type: 'unstyled',
+        depth: 0,
+        inlineStyleRanges: [],
+        entityRanges: [],
+        data: {} }],
+entityMap: {} },
+    schema: {
+      type: 'object',
+      title: 'empty object',
+      properties: {},
+    },
   },
   {
     id: 2,
     name: '模板二',
     style: '样式二',
+    editorState: { blocks: [{ key: '3b1pq', text: 'empty', type: 'unstyled', depth: 0, inlineStyleRanges: [], entityRanges: [], data: {} }], entityMap: {} },
+    schema: {
+      type: 'object',
+      title: 'empty object',
+      properties: {},
+    },
+  },
+  {
+    id: 3,
+    name: '模板三',
+    style: '样式三',
+    editorState: {
+      blocks: [{
+        key: '3b1pq',
+        text: 'example text',
+        type: 'unstyled',
+        depth: 0,
+        inlineStyleRanges: [{ offset: 2, length: 2, style: 'BOLD' }, { offset: 4, length: 3, style: 'ITALIC' }, { offset: 9, length: 2, style: 'COLOR-C0392B' }],
+        entityRanges: [],
+        data: {},
+      }],
+      entityMap: {},
+    },
+    schema: {
+      type: 'object',
+      title: 'empty object',
+      properties: {},
+    },
   },
 ];
 
 export default {
   'GET /template/all': templateData,
-  'POST /template': (req, res) => {},
+  'POST /template': (req, res) => { },
 };
