2164e0d06a16cea13590e3a4dae8bf48bd3b9cf8
TcPaulyue <1195882464@qq.com>
2019-07-16 10:14:48 +0800

    test

--- server/src/main/java/com/cvicse/leasing/model/Template.java
+++ server/src/main/java/com/cvicse/leasing/model/Template.java
@@ -1,28 +1,28 @@
-//package com.cvicse.leasing.model;
-//
-//
-//import lombok.Data;
-//import org.springframework.data.annotation.Id;
-//
-//
-//@Data
-//public class Template {
-//
-//    @Id
-//    public String id;
-//
-//    public String content;
-//
-//    public Template() {
-//    }
-//
-//    public Template(String content) {
-//        this.content = content;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return String.format("Contract[id=%s, content='%s']", id, content);
-//    }
-//
-//}
+package com.cvicse.leasing.model;
+
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+
+@Data
+public class Template {
+
+    @Id
+    public String id;
+
+    public String content;
+
+    public Template() {
+    }
+
+    public Template(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Contract[id=%s, content='%s']", id, content);
+    }
+
+}
