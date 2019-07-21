2d86fde29a56252f80df635d5531a35c681a3d0c
TcPaulyue <1195882464@qq.com>
2019-07-21 17:04:54 +0800

    reset server

--- server/src/main/java/com/cvicse/leasing/exception/ContractNotFoundException.java
+++ server/src/main/java/com/cvicse/leasing/exception/ContractNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Contract Not Found")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "ContractRequest Not Found")
 public class ContractNotFoundException extends Exception {
     private static final long serialVersionUID = 1L;
 
--- server/src/test/java/com/cvicse/leasing/service/TemplateServiceTests.java
+++ server/src/test/java/com/cvicse/leasing/service/TemplateServiceTests.java
@@ -41,19 +41,19 @@ public class TemplateServiceTests {
 //        repository.deleteAll();
 //        JSONObject jsonObject = new JSONObject();
 //        jsonObject.put("content","T1");
-//        Template template1=new Template(jsonObject);
+//        TemplateRequest template1=new TemplateRequest(jsonObject);
 //        template1.setId("1");
 //        JSONObject jsonObject1 = new JSONObject();
 //        jsonObject1.put("content","T2");
-//        Template template2 = new Template(jsonObject1);
+//        TemplateRequest template2 = new TemplateRequest(jsonObject1);
 //        template2.setId("2");
 //        JSONObject jsonObject2 = new JSONObject();
 //        jsonObject2.put("content","T2");
-//        List<Template> allTemplates = Arrays.asList(template1, template2);
+//        List<TemplateRequest> allTemplates = Arrays.asList(template1, template2);
 //        Mockito.when(repository.findById(template1.getId())).thenReturn(Optional.of(template1));
 //        Mockito.when(repository.findById(template2.getId())).thenReturn(Optional.of(template2));
 //        Mockito.when(repository.findById("wrong_id")).thenReturn(null);
-//        Mockito.when(repository.save(new Template(jsonObject2))).thenReturn(template2);
+//        Mockito.when(repository.save(new TemplateRequest(jsonObject2))).thenReturn(template2);
 //        Mockito.when(repository.findAll()).thenReturn(allTemplates);
 //    }
 //
@@ -61,21 +61,21 @@ public class TemplateServiceTests {
 //    public void given2Templates_when_getAllTemplates_thenReturn2Records() {
 //        JSONObject jsonObject = new JSONObject();
 //        jsonObject.put("content","T1");
-//        Template template1=new Template(jsonObject);
+//        TemplateRequest template1=new TemplateRequest(jsonObject);
 //
 //        JSONObject jsonObject1 = new JSONObject();
 //        jsonObject1.put("content","T2");
-//        Template template2 = new Template(jsonObject1);
+//        TemplateRequest template2 = new TemplateRequest(jsonObject1);
 //
 //
-//        List<Template> TemplateList= templateService.getAllTemplate();
-//        assertThat(TemplateList).hasSize(2).extracting(Template::getContent).contains(template1.getContent(),template2.getContent());
+//        List<TemplateRequest> TemplateList= templateService.getAllTemplate();
+//        assertThat(TemplateList).hasSize(2).extracting(TemplateRequest::getContent).contains(template1.getContent(),template2.getContent());
 //
 //    }
 //
 //    @Test
 //    public void givenRightTemplateId_thenReturnTemplate() throws TemplateNotFoundException {
-//        Template template =templateService.getTemplate("1");
+//        TemplateRequest template =templateService.getTemplate("1");
 //        assertThat(template).extracting("content").contains("T1");
 //    }
 //
@@ -83,8 +83,8 @@ public class TemplateServiceTests {
 //    public void createNewTemplate_thenReturnTheTemplate() throws TemplateNotFoundException{
 //        JSONObject jsonObject1 = new JSONObject();
 //        jsonObject1.put("content","T2");
-//        templateService.createTemplate(new Template(jsonObject1));
-//        Template template = templateService.getTemplate("2");
+//        templateService.createTemplate(new TemplateRequest(jsonObject1));
+//        TemplateRequest template = templateService.getTemplate("2");
 //        assertThat(template).extracting("content").contains("T2");
 //    }
 //
@@ -92,26 +92,26 @@ public class TemplateServiceTests {
 //    public void updateExistedTemplate_withExistedId_andNewName_thenReturnTheTemplate(){
 //        JSONObject jsonObject1 = new JSONObject();
 //        jsonObject1.put("content","T2");
-//        Template template =templateService.updateTemplate(jsonObject1,"1");
+//        TemplateRequest template =templateService.updateTemplate(jsonObject1,"1");
 //        assertThat(template.content.equals("T2")).isTrue();
 //    }
 
-    @Test
-    public void testUpdateHistory() throws TemplateNotFoundException{
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("content","T2");
-        Template template = templateService.createTemplate(new Template(jsonObject));
-        JSONObject jsonObject1 = new JSONObject();
-        jsonObject1.put("content","T3");
-        templateService.updateTemplate(jsonObject1,template.getId());
-        JSONObject jsonObject2 = new JSONObject();
-        jsonObject2.put("content","T4");
-        templateService.updateTemplate(jsonObject2,template.getId());
-       JSONArray jsonArray =templateService.trackTemplateChangesWithJavers(template.getId());
-       Template template1 = templateService.getTemplateWithJaversCommitId(template.getId(),"3");
-       System.out.println(template1.toString());
-        System.out.println(jsonArray);
-    }
+//    @Test
+//    public void testUpdateHistory() throws TemplateNotFoundException{
+//        JSONObject jsonObject = new JSONObject();
+//        jsonObject.put("content","T2");
+//        Template template = templateService.createTemplate(new Template(jsonObject));
+//        JSONObject jsonObject1 = new JSONObject();
+//        jsonObject1.put("content","T3");
+//        templateService.updateTemplate(jsonObject1,template.getId());
+//        JSONObject jsonObject2 = new JSONObject();
+//        jsonObject2.put("content","T4");
+//        templateService.updateTemplate(jsonObject2,template.getId());
+//       JSONArray jsonArray =templateService.trackTemplateChangesWithJavers(template.getId());
+//       Template template1 = templateService.getTemplateWithJaversCommitId(template.getId(),"3");
+//       System.out.println(template1.toString());
+//        System.out.println(jsonArray);
+//    }
 
 
 //    @Test
